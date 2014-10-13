77fa1f2e2ef7c647cff8d9e3aebb914db9dae712
BENATHMANE <ayoub.benathmane@usmba.ac.ma>
2014-10-13 16:58:38 +0200

    TeamForce

---
+++ webservices_TeamForce/src/main/java/fr/unice/polytech/soa1/TeamForce/business/Commande.java
@@ -0,0 +1,10 @@
+package fr.unice.polytech.soa1.TeamForce.business;
+
+import javax.xml.bind.annotation.XmlType;
+
+@XmlType
+public class Commande {
+
+//TO DO
+
+}
---
+++ webservices_TeamForce/src/main/java/fr/unice/polytech/soa1/TeamForce/business/Steps.java
@@ -0,0 +1,5 @@
+package fr.unice.polytech.soa1.TeamForce.business;
+
+public enum Steps {
+  Ordered, Payed, Prepared, Send;
+}
